07b9f417a901c632b7afe293b43b979672ef19f8
Keren-Happuc Fleurimond <keren@noukod.com>
2019-01-25 16:26:35 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 4) do
+ActiveRecord::Schema.define(version: 3) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
@@ -30,8 +30,6 @@ ActiveRecord::Schema.define(version: 4) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "artist_id"
-    t.integer  "genre_id"
   end
 
 end
