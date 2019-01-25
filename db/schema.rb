45c7f177289f06f9d54658a8d00a00f0de13a2f3
Keren-Happuc Fleurimond <keren@noukod.com>
2019-01-25 16:40:43 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -28,8 +28,9 @@ ActiveRecord::Schema.define(version: 3) do
 
   create_table "songs", force: :cascade do |t|
     t.string   "name"
+    t.integer  "artist_id"
+    t.integer  "genre_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
